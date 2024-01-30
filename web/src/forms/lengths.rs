50a6cae2b1790e637bac41ea9a295fe8952af517
drcreeper4 <trottier.charlesantoine@gmail.com>
2024-01-30 13:52:23 -0500

    Updated web/Cargo.toml, Updated web/src/forms/lengths.rs

--- web/src/forms/lengths.rs
+++ web/src/forms/lengths.rs
@@ -5,7 +5,7 @@ use yewdux::functional::{use_store, use_store_value};
 use svgtypes::LengthUnit;
 
 use crate::{
-    state::{AppState, FormState},
+    state::FormState,
     ui::*,
 };
 
@@ -20,7 +20,6 @@ macro_rules! length_input {
             paste! {
                 #[function_component([<$name Input>])]
                 pub fn [<$name:snake:lower _input>]() -> Html {
-                    let app_state = use_store_value::<AppState>();
                     let (form_state, form_dispatch) = use_store::<FormState>();
                     let children = html! (
                         <>
@@ -36,10 +35,32 @@ macro_rules! length_input {
                             <Opt value={"Percent"} children={html!("Percent")}/>
                         </>
                     );
+                    let onchange = form_dispatch.reduce_mut_callback_with(|state, event: Event| {
+                        let value = event.target_unchecked_into::<web_sys::HtmlSelectElement>().value();
+                        let parsed = match value.as_str() {
+                            "None" => LengthUnit::None,
+                            "Em" => LengthUnit::Em,
+                            "Ex" => LengthUnit::Ex,
+                            "Px" => LengthUnit::Px,
+                            "In" => LengthUnit::In,
+                            "Cm" => LengthUnit::Cm,
+                            "Mm" => LengthUnit::Mm,
+                            "Pt" => LengthUnit::Pt,
+                            "Pc" => LengthUnit::Pc,
+                            "Percent" => LengthUnit::Percent,
+                            _ => LengthUnit::Mm,
+                        };
+
+                        // Handle arrays
+                        $(
+                            let _ = $app_idx;
+                            let parsed = if value.is_empty() { None } else { Some(Ok(parsed)) };
+                        )?
+                        state.$form_accessor $([$form_idx])? = parsed;
+                    });
                     html! {
-                        // unwrap_or(&Ok(0.)) is just a macro hack to make None a valid state
                         <FormGroup success={form_state.$form_accessor $([$form_idx] .as_ref().unwrap_or(&Ok(LengthUnit::None)))?.is_ok()}>
-                            <Select label=$label desc=$desc children={children}/>
+                            <Select label=$label desc=$desc children={children} onchange={onchange}/>
                         </FormGroup>
                     }
                 }
